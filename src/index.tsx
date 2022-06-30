18a1916f368c9c27bf40b28093565901e3392a64
WelliWillers <wellington.willers@gmail.com>
2022-06-30 10:21:34 -0300

    Test dt money

--- src/index.tsx
+++ src/index.tsx
@@ -34,11 +34,11 @@ createServer({
     })
 
 
-    this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody);
+  //   this.post('/transactions', (schema, request) => {
+  //     const data = JSON.parse(request.requestBody);
 
-      return schema.create('transaction', data);
-    })
+  //     return schema.create('transaction', data);
+  //   })
 
   }
 })
